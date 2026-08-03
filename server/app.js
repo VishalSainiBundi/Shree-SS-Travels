6b5bcb60726d8b004b69d4a7d6e031d0f0b7e47c
VishalSainiBundi <your-email@example.com>
2026-08-03 17:19:38 +0530

    add cors

--- server/app.js
+++ server/app.js
@@ -2,10 +2,15 @@ const express= require('express')
 const connectDB = require('./connectDB')
 const dotenv =require('dotenv')
 const userRoute = require('./Routes/userRoute')
+const cors= require('cors')
 
 dotenv.config()
 const app= express()
 app.use(express.json())
+
+app.use(cors({
+    origin:"http://localhost:5173"
+}))
 
 app.use('/user', userRoute)
 
